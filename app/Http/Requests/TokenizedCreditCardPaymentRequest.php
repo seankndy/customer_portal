7f5a6d0ad123146a537d73fcbe5e072e972b9c2f
Gideon <gideonrich@hotmail.ca>
2020-12-17 10:08:55 -0700

    Add TokenizedCreditCardPaymentRequest

---
+++ app/Http/Requests/TokenizedCreditCardPaymentRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TokenizedCreditCardPaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'makeAuto' => 'boolean',
+            'amount' => 'required|numeric|min:0.01',
+            'customerId' => 'required|string',
+            'token' => 'required|string',
+            'identifier' => 'required|string',
+            'expirationDate' => 'required|string', //this has the / separator in it
+            'line1' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'string',
+            'zip' => 'required|string',
+            'country' => 'required|string',
+            'name' => 'required|string',
+        ];
+    }
+}
+
