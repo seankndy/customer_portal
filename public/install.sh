9f84c0f0c68fdf0db6ee13951bec939c60787177
Kevin Newman <kevin@kevinnewman.ca>
2021-01-26 13:22:20 -0500

    Whoops install change

--- public/install.sh
+++ public/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing PHP packages."
-(cd /usr/share/sonar; composer install -q --no-scripts --no-interaction --optimize-autoloader --no-progress)
+(cd /usr/share/public; composer install -q --no-scripts --no-interaction --optimize-autoloader --no-progress)
 
 echo "Installing JavaScript packages"
-(cd /usr/share/sonar; yarn install -s --prefer-offline --frozen-lockfile --non-interactive --cache-folder /var/www/.cache/yarn --emoji=true --ignore-optional)
+(cd /usr/share/public; yarn install -s --prefer-offline --frozen-lockfile --non-interactive --cache-folder /var/www/.cache/yarn --emoji=true --ignore-optional)
 
 echo "Current environment is set to '${APP_ENV}!'";
 
@@ -13,11 +13,11 @@ sudo /etc/my_init.d/99_init_sonar.sh
 php artisan config:cache
 
 # Run tmux
-chmod +x /usr/share/sonar/yarn.sh
+chmod +x /usr/share/public/yarn.sh
 tmux new-session \; \
-  send-keys 'bash /usr/share/sonar/hello.sh' C-m \; \
+  send-keys 'bash /usr/share/public/hello.sh' C-m \; \
   split-window -v \; \
-  send-keys '/usr/share/sonar/yarn.sh' C-m \; \
+  send-keys '/usr/share/public/yarn.sh' C-m \; \
   select-pane -t 0 \;
 
 sudo /etc/my_init.d/99_init_laravel.sh && cd /var/www/html && setuser www-data php artisan sonar:settingskey
